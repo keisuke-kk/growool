f993b5365ce4451afe828104514809931919bd0c
keisuke-kk <candy0220.k@gmail.com>
2020-08-03 22:09:54 +0900

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :application, 'growool'
 set :repo_url,  'git@github.com:keisuke-kk/growool.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
